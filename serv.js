e671dc39b9d8f151aca789be835e9f9ca2d358a1
dominsch <dominsch@gmail.com>
2023-10-26 23:10:57 +0000

    dfgd

--- serv.js
+++ serv.js
@@ -5,9 +5,13 @@ class Session {
         //this.instrument =
         this.activeDUT = 0
         this.activeRow = 1
+        this.activeEnd = 1
+        this.numEnds = 1
+        this.maxIL = 0.4
+        this.minRL = 55
+        this.WL = 1550
     }
     configure(firstSN, lastSN, numFibers, numEnds, maxIL, minRL) {
-        console.log("configuring", firstSN, lastSN, numFibers)
         this.firstSN = firstSN
         this.lastSN = lastSN
         this.numFibers = numFibers
@@ -19,7 +23,7 @@ class Session {
     makeDUTs() {
         this.DUTs = []
         for (let i = 0; i <= this.lastSN - this.firstSN; i++) {
-            this.DUTs[i] = new DUT(this.firstSN + i, 1, this.numFibers, [1550], true, 1, (i == 0))
+            this.DUTs[i] = new DUT(this.firstSN + i, this.numEnds, this.numFibers, [1550], true, 1, (i == 0))
         }
     }
     getDUT(sn) {
@@ -53,7 +57,8 @@ class DUT {
         this.ends = ends
         this.wavs = wavs
         this.hasrl = hasrl
-        this.focus = focus
+        this.focusFiber = focus
+        this.focusEnd = 1
         this.isActive = isActive
         this.IL = []
         this.RL = []
@@ -80,22 +85,34 @@ class DUT {
         })
     }
     next() {
-        if (this.focus >= this.fibers) {
-            this.focus = 1
-            return false
-        }
-        else {
-            this.focus++
+        if (this.focusEnd == this.ends) {
+           if (this.focusFiber >= this.fibers) {
+                this.focusFiber = 1
+                return false
+            }
+            else {
+                this.focusFiber++
+                if (this.focusEnd > 1) this.focusEnd--
+                return true
+            } 
+        } else {
+            this.focusEnd++
             return true
         }
     }
     prev() {
-        if (this.focus <= 1) {
-            this.focus = this.fibers
-            return false
-        }
-        else {
-            this.focus--
+        if (this.focusEnd == 1) {
+            if (this.focusFiber <= 1) {
+                this.focusFiber = this.fibers
+                return false
+            }
+            else {
+                this.focusFiber--
+                if (this.ends > 1) this.focusEnd++
+                return true
+            }
+        } else {
+            this.focusEnd--
             return true
         }
     }
@@ -139,11 +156,11 @@ const server = Bun.serve({
             console.log(scope)
             switch (scope) {
                 case "row":
-                    d.clearFiber(d.focus)
-                    return new Response(makeRow(d, d.focus, true));
+                    d.clearFiber(d.focusFiber)
+                    return new Response(makeRow(d, d.focusFiber, true));
                 case "dut":
                     d.clearAll()
-                    d.focus = 1
+                    d.focusFiber = 1
                     return new Response(makeCard(d, true));
                 case "all":
                     sess.getActiveDUT().focus = 1
@@ -159,29 +176,39 @@ const server = Bun.serve({
                     return new Response(res)
             }
         }
+        if (url.pathname === "/ping") {
+            let d = sess.getActiveDUT()
+            if (il < Math.abs(d.IL[d.focusEnd][d.focusFiber][d.wavs[0]])) d.IL[d.focusEnd][d.focusFiber][d.wavs[0]] = il
+            if (rl > d.RL[d.focusEnd][d.focusFiber][d.wavs[0]]) d.RL[d.focusEnd][d.focusFiber][d.wavs[0]] = rl
+            return new Response("pong", {
+                headers: { "HX-Trigger": "pong" }
+            })
+        }
         if (url.pathname === "/cap") {
             let d = sess.getActiveDUT()
-            if (il < Math.abs(d.IL[1][d.focus][d.wavs[0]])) d.IL[1][d.focus][d.wavs[0]] = il
-            if (rl > d.RL[1][d.focus][d.wavs[0]]) d.RL[1][d.focus][d.wavs[0]] = rl
-            return new Response(makeRow(d, d.focus, true))
+            if (il < Math.abs(d.IL[d.focusEnd][d.focusFiber][d.wavs[0]])) d.IL[d.focusEnd][d.focusFiber][d.wavs[0]] = il
+            if (rl > d.RL[d.focusEnd][d.focusFiber][d.wavs[0]]) d.RL[d.focusEnd][d.focusFiber][d.wavs[0]] = rl
+            return new Response(makeRow(d, d.focusFiber, true))
         }
         if (url.pathname === "/capend") {
             let d = sess.getActiveDUT()
-            let prevf = d.focus
+            let prevf = d.focusFiber
             if (!d.next()) {
                 let res = makeCard(sess.DUTs[sess.activeDUT], true, false)
                 sess.nextDUT()
                 res = res + makeCard(sess.DUTs[sess.activeDUT], true)
                 return new Response(res)
             }
-            let res = makeRow(d, prevf, true) + makeRow(d, d.focus, true)
+            let res = makeRow(d, prevf, true) + makeRow(d, d.focusFiber, true)
             return new Response(res)
         }
         if (url.pathname === "/next") {
             let d = sess.getActiveDUT()
-            let prevf = d.focus
+            console.log("next #1", d.ends, d.focusEnd, d.fibers, d.focusFiber)
+            let prevf = d.focusFiber
             d.next()
-            let res = makeRow(d, prevf, true) + makeRow(d, d.focus, true)
+            let res = makeRow(d, prevf, true) + makeRow(d, d.focusFiber, true)
+            console.log("next #2", d.ends, d.focusEnd, d.fibers, d.focusFiber)
             return new Response(res)
         }
         if (url.pathname === "/nextDUT") {
@@ -192,9 +219,9 @@ const server = Bun.serve({
         }
         if (url.pathname === "/prev") {
             let d = sess.getActiveDUT()
-            let prevf = d.focus
+            let prevf = d.focusFiber
             d.prev()
-            let res = makeRow(d, prevf, true) + makeRow(d, d.focus, true)
+            let res = makeRow(d, prevf, true) + makeRow(d, d.focusFiber, true)
             return new Response(res);
         }
         if (url.pathname === "/prevDUT") {
@@ -216,7 +243,35 @@ const server = Bun.serve({
             let sn = parseInt(url.searchParams.get('sn'))
             let row = parseInt(url.searchParams.get('fiber'))
             let d = sess.getDUT(url.searchParams.get('sn'))
-            return new Response(makeRow(d, row, 1));
+            return new Response(makeRow(d, row, true));
+        }
+        if (url.pathname === "/cell") {
+            let d = sess.getActiveDUT()
+            let end = parseInt(url.searchParams.get('end'))
+            let row = parseInt(url.searchParams.get('fiber'))
+            let wl = parseInt(url.searchParams.get('wl'))
+            let val = url.searchParams.get('content')
+            if (val != null && val != "" && val) console.log("old val", val)
+            let type = url.searchParams.get('type')
+            return new Response(makeCell(d, end, row, wl, type, val));
+        }
+        if (url.pathname === "/cellForm") {
+            let end = parseInt(url.searchParams.get('end'))
+            let row = parseInt(url.searchParams.get('fiber'))
+            let wl = parseInt(url.searchParams.get('wl'))
+            let type = url.searchParams.get('type')
+            return new Response(makeCellForm(end, row, wl, type))
+        }
+        if (url.pathname === "/cellSubmit") {
+            let sn = parseInt(url.searchParams.get('sn'))
+            let end = parseInt(url.searchParams.get('end'))
+            let row = parseInt(url.searchParams.get('fiber'))
+            let wl = parseInt(url.searchParams.get('wl'))
+            let type = url.searchParams.get('type')
+            let val = url.searchParams.get('content')
+            let d =sess.getDUT(sn)
+            console.log("A", end, row, wl, type)
+            return new Response(makeCell(d, end, row, wl, type, true, val))
         }
     },
 });
@@ -233,12 +288,14 @@ function makeTable(d, oob = false) {
     let sn = d.sn
     let out = `<thead>\n<tr>\n<td colspan = 10 id="P${sn}-T">P${sn}</td></tr><tr><th></th>`
     d.wavs.forEach(wl => {
-        out = out + `<th${(d.hasrl) ? " colspan = 2" : ""}>${wl}</th>`
+        out = out + `<th colspan = "${(d.hasrl) ? `${sess.numEnds * 2}` : `${sess.numEnds}`}">${wl}</th>`
     })
     out = out + `</tr><tr><th>Fiber</th>`
-    d.wavs.forEach(wl => {
-        out = out + `<th>IL</th>\n${(d.hasrl) ? "<th>RL</th>\n" : ""}`
-    })
+    for (let e = 1; e <= sess.numEnds; e++) {
+        d.wavs.forEach(wl => {
+            out = out + `<th>IL</th>\n${(d.hasrl) ? "<th>RL</th>\n" : ""}`
+        })
+    }
     out = out + `</tr></thead><tbody id="P${sn}-B">` + makeTBody(d) + `</tbody>`
     return out
 }
@@ -255,28 +312,30 @@ function makeTBody(d) {
 function makeRow(d, f, oob = false) {
     let sn = d.sn
     let n = d.wavs.length
-    //let out = `<tr class="${f}", id="${sn}-${f}" hx-post="/row" hx-trigger="click, sse:EventName, sse:event${f}" hx-swap="outerHTML">\n` `{"updateRow": {"sn": "1", "fiber": "4"}}`
-    // _="on click add .focused to next <tr/>"
-    let out = `<tr class="r${(f == d.focus && d.isActive) ? f % sess.base + " focused" : f % sess.base}" ` +
+    let out = `<tr class="r${(f == d.focusFiber && d.isActive) ? ((f-1) % sess.base+1) + " focused" : ((f-1) % sess.base+1)}" ` +
         `id="P${sn}-R${f}" ` +
-        `hx-get="/row?fiber=${f}" ` +
         `${(oob) ? `hx-swap-oob="true"` : ""} ` +
-        //`hx-trigger="click, updateRow[detail.sn=='${sn}'] from:body" ` +
         `hx-swap="outerHTML">\n` +
         `<td>${f}</td>\n`
-    for (let i = 1; i <= n; i++) {
-        out = out + makeCell(d, f, d.wavs[i - 1], "IL")
-        if (d.hasrl) {
-            out = out + makeCell(d, f, d.wavs[i - 1], "RL")
+    for (let e = 1; e <= sess.numEnds; e++) {
+        for (let i = 1; i <= n; i++) {
+            out = out + makeCell(d, e, f, d.wavs[i - 1], "IL")
+            if (d.hasrl) {
+                out = out + makeCell(d, e, f, d.wavs[i - 1], "RL")
+            }
         }
     }
     out = out + `</tr>\n`
     return out
 }
 
-function makeCell(d, f, wl, type, oob = false) {
-    let c
-    let content = (type == "IL") ? d.IL[1][f][wl] : d.RL[1][f][wl];
+function makeCell(d, e, f, wl, type, oob = false, value) {
+    let c, content
+    if(value) {
+        (type == "IL") ? d.IL[e][f][wl] = value : d.RL[e][f][wl] = value;
+    }
+    let id = "P" + d.sn + "-" + e + "-" + f + "-" + wl + "-" + type
+    content = (type == "IL") ? d.IL[e][f][wl] : d.RL[e][f][wl];
     if (content == -100) {
         content = ""
         c = "empty"
@@ -284,13 +343,23 @@ function makeCell(d, f, wl, type, oob = false) {
         (type == "IL" && content > sess.maxIL || type == "RL" && content < sess.minRL) ? c = "bad" : c = "good"
     }
     console.log(type, content, sess.maxIL, sess.minRL, c)
-    return `<td id="${"P" + d.sn + "-A" + f + "-" + wl + "-" + type}" ` +
-            `class="${c}">${content}</td>\n`
+    return `<td id="${id}" class="${c}"` +
+            `hx-get="/cellForm?end=${e}&fiber=${f}&wl=${wl}&type=${type}"` +
+            `hx-trigger="click"` +
+            `${(oob) ? ` hx-swap-oob="true" ` : ""}` +
+            `hx-swap="innerHTML" ` +
+            `>${content}</td>\n`
+}
+
+function makeCellForm(e, f, wl, type) {
+    return `<form class="cell_form" hx-get="/cellSubmit?end=${e}&fiber=${f}&wl=${wl}&type=${type}"` +
+            ` hx-swap="innerHTML" ` +
+            `><input class="cell_input" type="text" name="content" autofocus><input class="cell_submit" type="submit"></form> `
 }
 
 function makeCard(d, oob = false, active = d.isActive) {
     return `<li id="P${d.sn}-C" ` +
-        `class="item${d.sn - sess.firstSN + 1} ${(active) ? " center" : " hidden"} "` +
+        `class="card ${(active) ? " center" : " hidden"} "` +
         `${(oob) ? `hx-swap-oob="true"` : ""}>` +
         `<table hx-get="/tab" hx-vals='{"sn": "${d.sn}"}' hx-trigger="load" hx-swap="innerHTML">` +
         `</table></li>`
@@ -298,18 +367,29 @@ function makeCard(d, oob = false, active = d.isActive) {
 
 function makeForm() {
     return `<form hx-get="/form" hx-target="#tables" hx-swap="innerHTML">` +
-        `<label>First SN</label>` +
-        `<input type="number" name="firstSN" value="1">\n` +
-        `<label>Last SN</label>` +
-        `<input type="number" name="lastSN" value="10">\n` +
-        `<label>Number of Fibers</label>` +
-        `<input type="number" name="numFibers" value="6">\n` +
-        `<label>Number of Ends</label>` +
-        `<input type="number" name="numEnds" value="1">\n` +
-        `<label>Max IL</label>` +
-        `<input type="number" name="maxIL" value="0.4">\n` +
-        `<label>Min RL</label>` +
-        `<input type="number" name="minRL" value="55">\n` +
+        `<p><label>First SN</label>` +
+        `<input type="number" name="firstSN" value="${sess.firstSN}"></p>\n` +
+        `<p><label>Last SN</label>` +
+        `<input type="number" name="lastSN" value="${sess.lastSN}"></p>\n` +
+        `<p><label>Number of Fibers</label>` +
+        `<input type="number" name="numFibers" value="${sess.numFibers}"></p>\n` +
+        `<p><label>Number of Ends</label>` +
+        `<input type="number" name="numEnds" value="${sess.numEnds}"></p>\n` +
+        `<p><label>Max IL</label>` +
+        `<input type="number" name="maxIL" value="${sess.maxIL}"></p>\n` +
+        `<p><label>Min RL</label>` +
+        `<input type="number" name="minRL" value="${sess.minRL}"></p>\n` +
+        `<fieldset>
+        <legend>Wavelength</legend>
+        <p><input type="radio" id="850" name="wl" value="850" checked />
+            <label>850</label></p>
+            <input type="radio" id="1310" name="wl" value="1310" />
+            <label for="1310">1310</label>
+            <input type="radio" id="1550" name="wl" value="1550" />
+            <label for="1550">1550</label>
+            </fieldset>` +
         `<button class="btn">Submit</button>` +
         `</form>`
 }
+
+
