4b53efb36364e832a16d0d0f0873c83d5f1d6ca5
dominsch <dominsch@gmail.com>
2024-04-30 16:03:01 -0300

    cleanup

--- pm2.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    name: "test", // Name of your application
-    script: "test.js", // Entry point of your application
-    interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
-  };
